6c191c1d4e2eb7cd10f6fc009934c8eaf0a41f40
NorthernBlow <rheinhart@yandex.ru>
2025-04-12 11:38:58 +0300

    Add config for vm

---
+++ main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "~> 0.7.0"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+# Укажи путь к ISO или QCOW2 образу Ubuntu
+variable "os_image" {
+  default = "/var/lib/libvirt/images/ubuntu-22.04-server-cloudimg-amd64.img"
+}
+
+resource "libvirt_volume" "ubuntu_disk" {
+  name   = "ubuntu-docker.qcow2"
+  pool   = "default"
+  source = var.os_image
+  format = "qcow2"
+}
+
+resource "libvirt_cloudinit_disk" "cloudinit" {
+  name           = "cloudinit.iso"
+  pool           = "default"
+  user_data      = data.template_file.user_data.rendered
+  network_config = data.template_file.network_config.rendered
+}
+
+data "template_file" "user_data" {
+  template = file("${path.module}/cloud-init/user-data.yaml")
+}
+
+data "template_file" "network_config" {
+  template = file("${path.module}/cloud-init/network-config.yaml")
+}
+
+resource "libvirt_domain" "docker_vm" {
+  name   = "docker-builder"
+  memory = "4096"
+  vcpu   = 2
+
+  disk {
+    volume_id = libvirt_volume.ubuntu_disk.id
+  }
+
+  cloudinit = libvirt_cloudinit_disk.cloudinit.id
+
+  network_interface {
+    network_name = "default"
+  }
+
+  graphics {
+    type        = "vnc"
+    listen_type = "address"
+    autoport    = true
+  }
+}
+
